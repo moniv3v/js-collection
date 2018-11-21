3dc1f18d407f20dfe10b04809ab0f47a86969d56
Monique <moniquechan1220@gmail.com>
2018-11-21 14:53:49 +0800

    Updated map

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,17 +1,6 @@
 'use strict';
 
-function times2(element){
-  return element*2;
-}
-
-function map(collection,fn){
-  for(let i=0;i<collection.length;i++){
-    collection[i] = fn(collection[i]);
-  }
-  return collection;
-}
-
 function map_to_even(collection){
-  return map(collection,times2);
+  return collection.map(element=>element *2);
 }
 module.exports = map_to_even;
--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,17 +1,7 @@
 'use strict';
-function times4Plus1(element){
-  return (element*4)+1;
-}
-
-function map(collection,fn){
-  for(let i=0;i<collection.length;i++){
-    collection[i] = fn(collection[i]);
-  }
-  return collection;
-}
 
 var map_to_four_multiples_add_one = function(collection){
-  return map(collection,times4Plus1);
+  return collection.map(element=>(element*4)+1);
 };
 
 module.exports = map_to_four_multiples_add_one;
