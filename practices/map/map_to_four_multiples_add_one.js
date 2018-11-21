67eeff8922d21bf0e8f09a4ca327b9d2c651c965
Monique <moniquechan1220@gmail.com>
2018-11-21 14:50:27 +0800

    completed four_multiples_add_one

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,17 @@
 'use strict';
+function times4Plus1(element){
+  return (element*4)+1;
+}
+
+function map(collection,fn){
+  for(let i=0;i<collection.length;i++){
+    collection[i] = fn(collection[i]);
+  }
+  return collection;
+}
+
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+  return map(collection,times4Plus1);
 };
 
 module.exports = map_to_four_multiples_add_one;
